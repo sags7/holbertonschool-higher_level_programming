8549c8480a949ab7fc03b94a0dabd7b708e95f29
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-04 16:15:24 -0500

    Feature: add 3-print_alphabt.py that prints part of the alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("".join("{}".format(chr(i)) for i in range(97, 123) if chr(i) not in ("e", "q")), end="")
+print(
+    "".join("{}".format(chr(i)) for i in range(97, 123) if chr(i) not in ("e", "q")),
+    end="",
+)
