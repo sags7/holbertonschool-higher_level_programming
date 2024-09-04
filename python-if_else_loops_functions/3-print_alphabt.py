4423af7646d10d61d4c31847c826e37ecb013a22
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-04 16:17:46 -0500

    Update: fix pycodestyle

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 print(
-    "".join("{}".format(chr(i)) for i in range(97, 123) if chr(i) not in ("e", "q")),
+    "".join("{}".format(chr(i))
+            for i in range(97, 123) if chr(i) not in ("e", "q")),
     end="",
 )
