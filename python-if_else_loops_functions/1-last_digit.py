ef5136a1bc19a24bcd8e024f0ab05b379f62c22e
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-04 14:15:45 -0500

    Feature: add 1-last_digit.py that prints a message to the console

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+finalStr = ""
+if number > 5:
+    finalStr = "and is greater than 5"
+elif number == 0:
+    finalStr = "and is zero"
+elif number < 6:
+    finalStr = "and is less than 6 and not 0"
+
+lastDigit = ""
+if number >= 0:
+    lastDigit = number % 10
+else:
+    lastDigit = -(-number % 10)
+
+print("Last digit of {} is {} {}".format(number, lastDigit, finalStr))
