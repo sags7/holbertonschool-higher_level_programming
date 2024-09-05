21fb197c4d4e21a14c7e7c3a9a3ca233e04c4c87
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-05 11:54:12 -0500

    Update: fix output format to meet requirement

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,7 @@ for i in range(10):
     for j in range(10):
         if i >= j:
             continue
+        elif i == 8 and j == 9:
+            print(89)
         else:
-            print("{:02d}".format((i * 10) + j))
+            print("{:02d}, ".format((i * 10) + j), end="")
