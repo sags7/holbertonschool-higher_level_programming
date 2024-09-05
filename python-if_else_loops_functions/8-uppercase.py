73b788e56881e83839d7c6daafdf22b5b65d4b3e
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-05 12:24:16 -0500

    Feature: uppercase(str) prints str in all caps

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    for c in str:
+        print(
+            "{}".format(chr(ord(c) - 32) if ord(c) >= 97 and ord(c) <= 123
+                        else c),
+            end="",
+        )
+    print("")
