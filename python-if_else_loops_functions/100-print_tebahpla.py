39319995515c2b155432e830a437cad05d74ee9d
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-05 13:01:42 -0500

    Update: reformatted code to comply with reqs

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(122, 96, -1):
-    print(chr(i) if i % 2 == 0 else chr(i - 32), end="")
+print(
+    "".join(f"{chr(i) if i % 2 == 0 else chr(i - 32)}"
+            for i in range(122, 96, -1)),
+    end="",
+)
