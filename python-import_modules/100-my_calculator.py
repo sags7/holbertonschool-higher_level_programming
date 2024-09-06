1e462477f4a4ece8ea948d342d28e6c288efd9bc
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-06 14:17:28 -0500

    Feature: change order of execution

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,7 +4,9 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 
 
-def my_calc(a, b):
+if __name__ == "__main__":
+    a = int(argv[1])
+    b = int(argv[3])
     arlen = len(argv) - 1
     op = argv[2]
 
@@ -22,7 +24,3 @@ def my_calc(a, b):
         print(f"{a} * {b} = {mul(a, b)}")
     elif op == "/" and b != 0:
         print(f"{a} / {b} = {div(a, b)}")
-
-
-if __name__ == "__main__":
-    my_calc(int(argv[1]), int(argv[3]))
