5ee2aaa2a810c11ac063e17dc65210e451548a81
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-06 14:19:48 -0500

    Feature: change order of execution

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,17 +5,19 @@ from calculator_1 import add, sub, mul, div
 
 
 if __name__ == "__main__":
-    a = int(argv[1])
-    b = int(argv[3])
     arlen = len(argv) - 1
-    op = argv[2]
 
     if arlen != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit()
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
     if op not in {"+", "-", "*", "/"}:
         print("Unknown operator. Available operators: +, -, * and /")
         exit()
+
     if op == "+":
         print(f"{a} + {b} = {add(a, b)}")
     elif op == "-":
