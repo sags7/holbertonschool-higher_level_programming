9e35a1c65cd74b532ac1a6b7275c719ff64943d5
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-09 17:03:46 -0500

    Update: an empty row now prints a newline

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,12 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            print("{:d}".format(row[i]), end="")
-            if i < len(row) - 1:
-                print(" ", end="")
-            else:
-                print()
+        if len(row) == 0:
+            print()
+        else:
+            for i in range(len(row)):
+                print("{:d}".format(row[i]), end="")
+                if i < len(row) - 1:
+                    print(" ", end="")
+                else:
+                    print()
