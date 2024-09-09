726d923777d5bbed2effeb7f8e0fdc9e906c54ee
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-09 17:39:13 -0500

    Update: fix typo in variable name

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 
-def print_list_integer(MyList=[]):
-    print("".join("{:d}\n".format(item) for item in MyList), end="")
+def print_list_integer(myList=[]):
+    print("".join("{:d}\n".format(item) for item in myList), end="")
