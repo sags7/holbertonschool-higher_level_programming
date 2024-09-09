2f58fdd9da63b6ad320d81cc495398a4717d0529
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-09 17:23:08 -0500

    Feature: add_tuple() returns a 2 elmnt tuple that is the addition of 2 input tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,4 +4,4 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     aTuple = (tuple_a + (0, 0))[:2]
     bTuple = (tuple_b + (0, 0))[:2]
-    return ((aTuple[1] + bTuple[1]), (aTuple[2] + bTuple[2]))
+    return ((aTuple[0] + bTuple[0]), (aTuple[1] + bTuple[1]))
