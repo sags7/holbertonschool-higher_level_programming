ac54c81a05ad80c2e490210e5e4660f207a8b734
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-11 11:51:19 -0500

    Update: fix pycodestyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(myList=[], x=0):
         for i in range(x):
             print("{}".format(myList[i]), end="")
             printed += 1
-    except:
+    except IndexError:
         pass
     print()
     return printed
