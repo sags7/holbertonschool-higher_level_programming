dd4f052b95c6e0886d61d89339532cc2a93e41f4
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-11 11:49:16 -0500

    Update: remove the space after each printed object

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(myList=[], x=0):
     printed = 0
     try:
         for i in range(x):
-            print("{} ".format(myList[i]), end="")
+            print("{}".format(myList[i]), end="")
             printed += 1
     except IndexError:
         print("Index out of bounds")
