676c9f3fa56509add9d34af319d23b64ec0bdbb4
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-11 12:48:17 -0500

    Fix: fix uppercase R in result message

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -8,5 +8,5 @@ def safe_print_division(a, b):
     except (TypeError, ValueError, ZeroDivisionError):
         pass
     finally:
-        print("Inside Result: {}".format(c))
+        print("Inside result: {}".format(c))
     return c
