6daef538765570a00e131feed7e00aec2e586e0b
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-11 15:14:08 -0500

    Feature: 102-magic_calculation.py replicates the Bytcode provided

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/user/bin/python3
+
+
+def magic_calculations(a, b):
+    result = 0
+    try:
+        for i in range(1, 3):
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += (a**b) / i
+    except Exception:
+        result = b + a
+    return result
