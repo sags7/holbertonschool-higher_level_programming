b835196bc13ed32184c438b6330ab4ef21ed32a7
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-11 13:19:05 -0500

    Feature: list_division(list, list, length) returns a list with the division results

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def list_division(myList1, myList2, listLength):
+    retVal = []
+    for i in range(listLength):
+        try:
+            retVal.append(myList1[i] / myList2[i])
+        except TypeError:
+            print("wrong type")
+            retVal.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            retVal.append(0)
+        except IndexError:
+            print("out of range")
+            retVal.append(0)
+    print(retVal)
+    return retVal
