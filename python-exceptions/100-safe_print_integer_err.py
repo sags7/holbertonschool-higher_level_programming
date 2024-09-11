64a30523aefeff8e1da1afc000a84aaa835e456d
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-11 14:11:56 -0500

    Feature:safe_print_err() sends err messages to stdout

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(int(value)))
         return True
     except TypeError as e:
         print("Exception: {e}", file=sys.stderr)
