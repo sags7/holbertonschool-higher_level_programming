b52b2c58f6ff4609b555763121e2a2b977e46b49
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-16 17:49:03 -0500

    Feature: text_indentation(str) prints a string, replacing .?: for two empty lines

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+this module provides a function that prints a text with
+2 new lines after each of these characters '.?:'
+"""
+
+
+def text_indentation(text):
+    """
+    text_indentation prints a text to the console, adding
+    2 new lines after each of these characters '.?:'
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for character in text:
+        if character in ".?:":
+            print(f"{character}\n")
+        else:
+            print(character, end="")
