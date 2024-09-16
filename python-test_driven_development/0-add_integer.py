8093b9396bcb06224c61e6be07f9f8b78273eade
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-16 11:34:40 -0500

    Documentation: updated docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-add_integer adds two integers and returns the result. For example,
-
->>> add_integer(5, 4)
-9
-
-Args:
-    a (int, float): first param.
-    b (int, float): second param, defaults to 98
-Returns:
-    The sum of a and b as an int
-"""
 
 
 def add_integer(a, b=98):
+    """
+    add_integer adds two integers and returns the result. For example,
 
+    >>> add_integer(5, 4)
+    9
+
+    Args:
+        a (int, float): first param.
+       b (int, float): second param, defaults to 98
+    Returns:
+        The sum of a and b as an int
+    """
+    
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
