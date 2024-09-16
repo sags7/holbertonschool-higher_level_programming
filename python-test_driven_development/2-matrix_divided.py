2a771a2c63034ade2af68db4a7392e85e7a9a5d6
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-16 14:37:20 -0500

    Documentation: update doctest

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -20,14 +20,15 @@ def matrix_divided(matrix, div):
         a new matrix with the result of the division
         of each element of matrix by div
     """
+    err = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(err)
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(err)
 
     retVal = []
     for row in matrix:
@@ -35,14 +36,11 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError(
-                    "matrix must be a matrix (list of lists)\
-                        of integers/floats"
-                )
+                raise TypeError(err)
 
     retVal = [[round((element / div), 2) for element in row] for row in matrix]
 
     if not all(isinstance(e, (int, float)) for e in row for row in retVal):
-        TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        TypeError(err)
 
     return retVal
