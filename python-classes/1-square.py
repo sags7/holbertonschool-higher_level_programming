de79cdcd78de66dbe371bd43d6db6f6bd0e0d660
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-18 14:23:58 -0500

    Update: removed getter for size attribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -19,21 +19,4 @@ class Square:
         Args:
             size (int, float): the size of the square, default is 0
         """
-
         self.size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, size):
-        """validates and sets the size of the square"""
-
-        if not isinstance(size, (float, int)):
-            raise TypeError("size must be a number")
-
-        if size >= 0:
-            self.__size = size
-        else:
-            raise ValueError("size must be a positive number")
