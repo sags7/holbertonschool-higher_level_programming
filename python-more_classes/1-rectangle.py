6900a364a47cfae2d414a010bdac4e51fb41234c
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-19 16:29:30 -0500

    Fix: the __init__ isnt using the setters

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initializes the instance with optional width and height"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
