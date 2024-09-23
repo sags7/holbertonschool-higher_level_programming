14ab3159b78156e1401aef3a111661c840f71a06
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-23 13:12:12 -0500

    Feature: lookup(obj) returns a list with the attributes of obj

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""This modules provides a definition of the lookup() method"""
 
 
 def lookup(myobject):
-    """returns a list with all the attributes of the passed object"""
+    """returns a list with all the attributes of the passed object
+    Args:
+        myobject (object): the object of which to return a list of attributes
+    """
     return dir(myobject)
