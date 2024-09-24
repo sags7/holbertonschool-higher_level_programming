ff2b127c890d6a9c57b4f1e1d93f34d62554c309
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-24 16:26:49 -0500

    Update: removed BaseGeometry definition from 8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,19 +2,7 @@
 """Module provides the definition of the Rectangle and BaseGeometry classes"""
 
 
-class BaseGeometry:
-    """Definition of the BaseGeometry class"""
-
-    def area(self):
-        """raises an Exception as it is yet unimplemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """makes sure that value is of the correct type and value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
