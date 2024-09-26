5008c140ec7e82379c9a103c6bf86f3cd12231c2
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-26 09:22:54 -0500

    Feature: Rectanngle now implements area() and __str__ mthods

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module provides the definition of the Rectangle and BaseGeometry classes"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a Rectangle class derived from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initializes the Rectangle with width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
