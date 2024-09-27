e8d7f68dae8c6fd7f266163d5909d27d4643df03
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-09-26 21:50:10 -0500

    Update: Circle class handles negative radii

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -25,7 +25,7 @@ class Circle(Shape):
     def __init__(self, radius):
         """initialization of Circle"""
         if radius < 0:
-            return None
+            raise TypeError("Radius needs to be positive")
         self.__radius = radius
 
     def area(self):
