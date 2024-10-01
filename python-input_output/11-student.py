609cfb6978d44e60be35bc1445adcca74b1ad3e3
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-10-01 18:40:30 -0500

    Feature: 11-student.py implements reload from json functionality

---
+++ python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+provides a definition for the Student class
+"""
+
+
+class Student:
+    """definition of the Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        returns a dictionary with all
+        the attrs that can be serialized as JSON
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            retVal = {}
+            if isinstance(attrs, list):
+                if all(isinstance(attr, str) for attr in attrs):
+                    for atr in attrs:
+                        if atr in self.__dict__:
+                            retVal[atr] = self.__dict__[atr]
+            return retVal
+
+    def reload_from_json(self, json):
+        """
+        replaces attributes in the instance with
+        those found in the json
+        """
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
