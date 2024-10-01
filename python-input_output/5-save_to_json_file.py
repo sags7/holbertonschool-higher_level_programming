8eb9bd2046756923c216cadf540226aafb694c95
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-10-01 14:36:21 -0500

    Fix: incorrect call to dump on line 10

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
     with open(filename, "w", encoding="utf-8") as f:
-        f.dump(my_obj, filename)
+        json.dump(my_obj, f)
