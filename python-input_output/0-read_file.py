4a583a882d848dd6e8d293042334dd43060298f7
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-10-01 13:49:16 -0500

    Update: removed newline character after print

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """read_file(filename) prints the the content of filename to stdout"""
 
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
