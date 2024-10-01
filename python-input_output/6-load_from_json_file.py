7be71621ad15bc79c94d64ef57b8619bc146ef5b
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2024-10-01 14:44:31 -0500

    Feature: load_from_json_file(fileName) returns the contents of the file as a python object

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""provides the definition for load_from_json_file()"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a JSON file"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.loads(f)
