a8548d7491089374ca473382720fbdd8036c8f35
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-02-17 13:01:33 -0500

    changed 404 not found message to meet expected output

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -36,7 +36,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
