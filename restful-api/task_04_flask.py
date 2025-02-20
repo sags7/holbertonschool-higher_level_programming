ddd253673dc26c021c5e0f534445119466c08861
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-02-20 17:39:56 -0500

    changed user not found error to meet criteria

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -39,7 +39,7 @@ def get_user(username):
     if username in users:
         return jsonify(users[username])
     else:
-        return jsonify({"error": f"{username} not found"}), 404
+        return jsonify({"error": "User not found"}), 404
 
 
 @app.route("/add_user", methods=["POST"])
