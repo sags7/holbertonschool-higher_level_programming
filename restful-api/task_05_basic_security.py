79b95200f67c26c7cb8aab23ace69adab51c5049
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-02-23 15:22:15 -0500

    removed argument from basic_protected() method

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -49,7 +49,7 @@ def verify_password(username, password):
         return user
 
 
-@app.route("/basic-protected", methods=["GET"])
+@app.route("/basic-protected")
 @auth.login_required
 def basic_protected():
     return "Basic Auth: Access Granted"
