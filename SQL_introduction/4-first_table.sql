867dd93845b09460db42b9d1325cca4bf83cf9b4
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-06 11:32:05 -0500

    fixed table to fit criteria

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,6 @@
 -- creates a table called first_table in the current database
 CREATE TABLE IF NOT EXISTS first_table(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    score INT NOT NULL,
+    id INT AUTO_INCREMENT,
+    name VARCHAR(256),
     PRIMARY KEY (id)
 );
