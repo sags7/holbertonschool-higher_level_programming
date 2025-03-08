e6c1651c9efbfc628370372d2315ed58f2185359
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-08 13:04:50 -0500

    Fix: formatting

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,15 +1,10 @@
 -- script lists all cities of California that can be found in the database hbtn_0d_usa
-SELECT
-    id,
-    name
-FROM
-    cities
-WHERE
-    state_id = (
-        SELECT
-            name = "California"
-        FROM
-            states
-    )
-ORDER BY
-    id ASC;
+SELECT id, name FROM cities
+    WHERE
+        state_id IN (
+            SELECT
+                name = "California"
+            FROM
+                states
+        )
+ORDER BY id ASC;
