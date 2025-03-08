8f287f31752dc4be00ab0324dec60ac1746eb5de
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-08 10:45:42 -0500

    added task1 solution

---
+++ SQL_more_queries/1-create_users.sql
@@ -0,0 +1,4 @@
+-- creates a MySQL user and grants him privileges on a database.
+
+CREATE USER IF NOT EXISTS 'user_0d_1' @ 'localhost' IDENTIFIED BY 'user_0d_1_password';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @ 'localhost';
