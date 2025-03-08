b0f9db4c61cd6520815698589e581a57955e8b79
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-08 10:52:36 -0500

    fix: removed spaces arounds the @s

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates a MySQL user and grants him privileges on a database.
 
-CREATE USER IF NOT EXISTS 'user_0d_1' @ 'localhost' IDENTIFIED BY 'user_0d_1_password';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @ 'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_password';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
