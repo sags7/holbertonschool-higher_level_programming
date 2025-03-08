0de3b99498ea835ecb4fb1aeecb631f8123913e3
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-08 12:55:31 -0500

    fix: changed quotes for "

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,13 +1,2 @@
 --script lists all cities of California that can be found in the database hbtn_0d_usa
-SELECT
-    id,
-    name
-FROM
-    cities
-WHERE
-    state_id IN (
-        SELECT
-            name = 'California'
-        FROM
-            states
-    );
+SELECT id, name FROM cities WHERE state_id IN (SELECT name = "California" FROM states);
