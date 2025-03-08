99c4ef7bc0e978a59fb13486da15cafc90e86f3a
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-08 11:53:58 -0500

    added solution for task7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,6 +5,7 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
-    name VARCHAR(256) NOT NULL
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL,
 );
