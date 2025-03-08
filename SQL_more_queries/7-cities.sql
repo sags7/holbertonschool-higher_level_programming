0f323829c947b482ec42262194ced8d3406a97e0
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-08 12:37:48 -0500

    specified db.table, removed creation of states

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,18 +3,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS states(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-);
-
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");
-
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT PRIMARY KEY AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
-
-INSERT INTO cities (state_id, name) VALUES (1, "San Francisco");
