bd9cc2c2703f0c8e324c4a14eefdafc69e79a434
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-08 11:30:45 -0500

    change: removed id as primary key

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,5 @@
 -- script creates a table on the MySQL server
 CREATE TABLE IF NOT EXISTS force_name(
     id INT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    name VARCHAR(256) NOT NULL
 );
