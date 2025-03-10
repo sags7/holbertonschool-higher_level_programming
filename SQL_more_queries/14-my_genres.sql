7a43b695e72668307ca6008c74e13bd97c239a91
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 13:31:57 -0500

    added solution for task14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,7 @@ SELECT
 FROM
     tv_genres
     JOIN tv_show_genres ON tv_genres.id = tv_shows_genres.genre_id
-    JOIN tv_show ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE
     tv_shows.title = 'Dexter';
 
