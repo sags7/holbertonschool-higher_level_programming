da4c8f45e49f6169477c0041c4f0e44ecec73561
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 12:09:19 -0500

    fixed typo in tv_shows table name

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
     FROM tv_shows LEFT JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
-    WHERE tv_show.genre_id IS NULL
+    WHERE tv_shows.genre_id IS NULL
     ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
