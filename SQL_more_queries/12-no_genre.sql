3876212bd7919a8e239ab984800ce0697050a42a
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 12:07:49 -0500

    added solution for task12

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- script lists all shows that don't have a genre linked to them and orders them by title and genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+    FROM tv_shows LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_show.genre_id IS NULL
+    ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
