be904837f978300d9630eb175207710b571e29d5
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 14:22:46 -0500

    added solution for task16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- this script lists all shows and genres linked to that show from the database
 
-SELECT tv_shows.title, tv_genres.name FROM tv_shows, tv_genres
-    JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-    JOIN tv_show_genres ON tv_show_genres.genre_id = tv.genres.id
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+    LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+    LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
     ORDER BY tv_shows.title ASC, tv_genres.name ASC;
