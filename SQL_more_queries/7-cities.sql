6ff442c42ca5c33e20e124ffb656a58e28f22f59
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 14:50:48 -0500

    removed USE keyword

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- Script creates a database and a table within it
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `state_id` INT NOT NULL,
-    `name` VARCHAR(256) NOT NULL,
-    FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`)
+    FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`),
+    `name` VARCHAR(256) NOT NULL
 );
