463a2a76ce8feed3e0522ae0e197eefbba4272b6
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 14:34:34 -0500

    added use keyowrd

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 --Script creates a database and a table within it
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `cities` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `state_id` INT NOT NULL,
+    `name` VARCHAR(256) NOT NULL,
+    FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)
 );
