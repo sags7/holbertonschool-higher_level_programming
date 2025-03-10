7e4ad4bdecc5b2fd4c68f406448ecfa5f7638cb2
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 14:41:49 -0500

    added use keyowrd

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
---Script creates a database and a table within it
+-- Script creates a database and a table within it
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `cities` (
