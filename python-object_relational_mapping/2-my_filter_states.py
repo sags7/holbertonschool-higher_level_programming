6a0420f28549e719712c751dca611a86d5bc4e84
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 19:31:25 -0500

    fix: tuple in execute statement

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM `states`\
-                WHERE `states`.`name` = %s", (sys.argv[4])
+                WHERE `states`.`name` = %s", (sys.argv[4],)
                 )
     rows = cur.fetchall()
     for row in rows:
