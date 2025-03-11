61710d028cf5819cc4fbf11507db5b9a45b53192
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 20:05:03 -0500

    added solution for task4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -25,7 +25,9 @@ if __name__ == "__main__":
         charset="utf8"
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM `cities` ORDER BY `cities`.`id` ASC")
+    cur.execute("SELECT `id`, `state`, `name` FROM `cities`\
+                JOIN `states` ON `states`.`id = `cities`.`state_id`\
+                ORDER BY `cities`.`id` ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
