5c603d2433b38294902feacdbb1d4fb29c04f7ae
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 19:47:05 -0500

    added solution for task3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Once again, write a script that takes in arguments
+and displays all values in the states table of
+hbtn_0e_0_usa
+where name matches the argument. But this time, write one
+that is safe from MySQL injections!
+
+Your script should take 4 arguments: mysql username, mysql password,
+database name and state name searched (safe from MySQL injection)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute("SELECT `states`.`id`, `states`.`name` FROM states\
+                WHERE BINARY `states`.`name` = %s", (sys.argv[4],)
+                )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
