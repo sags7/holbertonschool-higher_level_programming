24f756dd30095bac9165d24a82a845bacfe9ee87
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-11 15:26:10 -0500

    fix: added like() method

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -28,6 +28,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(State.name == "%a%"):
+    for state in session.query(State).filter(State.name.like("%a%")):
         session.delete(state)
     session.commit()
