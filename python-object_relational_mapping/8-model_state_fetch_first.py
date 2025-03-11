ba22699c6814bf817704157ee6267d6d71fb5675
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-11 13:16:44 -0500

    fixed -for- on a non iterable

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -32,8 +32,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id).first():
-        if not state:
-            print("Nothing\n")
-        else:
-            print("{}: {}".format(state.id, state.name))
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing\n")
