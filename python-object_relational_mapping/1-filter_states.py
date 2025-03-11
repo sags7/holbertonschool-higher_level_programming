c7e5ec2eb785272aef70150b8a02cfab2d686ec4
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 19:11:18 -0500

    fixed: longer lines to be under 79 characters

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,7 @@
 Write a script that lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa:
 
-Your script should take 3 arguments: mysql username, 
+Your script should take 3 arguments: mysql username,
 mysql password and database name (no argument validation needed)
 You must use the module MySQLdb (import MySQLdb)
 Your script should connect to a MySQL server running on localhost at port 3306
@@ -33,8 +33,8 @@ def filter_states():
     cur = db.cursor()
 
     cur.execute("SELECT `states`.`id`, `states`.`name` "
-    "FROM `states` WHERE `states`.`name` "
-    "LIKE BINARY 'N%' ORDER BY `states`.`id` ASC"
+        "FROM `states` WHERE `states`.`name` "
+        "LIKE BINARY 'N%' ORDER BY `states`.`id` ASC"
                 )
     rows = cur.fetchall()
     for row in rows:
