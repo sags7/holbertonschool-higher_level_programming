6f905ec6c500330e63074600a4d152672b076d61
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-11 17:15:45 -0500

    added solution to task14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+write a script 14-model_city_fetch_by_state.py that
+prints all City objects from the database hbtn_0e_14_usa:
+
+Your script should take 3 arguments: mysql username,
+mysql password and database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state -
+from model_state import Base, State
+Your script should connect to a MySQL server running
+on localhost at port 3306
+Results must be sorted in ascending order by cities.id
+Results must be display as they are in the example below
+(<state name>: (<city id>) <city name>)
+Your code should not be executed when imported
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3],
+        pool_pre_ping=True
+    ))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # state name, city id, city name
+    for city in session.query(City, State)\
+            .filter(City.state_id == State.id).order_by(City.id).all():
+        print("{}: ({}) {}"
+              .format(city.State.name, city.City.id, city.City.name))
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Write a Python file similar to model_state.py
+named model_city.py that contains the class definition of a City.
+
+City class:
+inherits from Base (imported from model_state)
+links to the MySQL table cities
+class attribute id that represents a column of an auto-generated,
+unique integer, can’t be null and is a primary key
+class attribute name that represents a column of a string of
+128 characters and can’t be null
+class attribute state_id that represents a column of an integer,
+can’t be null and is a foreign key to states.id
+You must use the module SQLAlchemy
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    City object for a MySQL database
+    __tablename__ (str): The name of the MySQL table to store Cities
+    id (sqlalchemy.Integer): The city's id
+    name (sqlalchemy.String): The city's name
+    state_id (sqlalchemy.Integer): The city's state id
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_jey=True, nullable=False, auto_increment=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
