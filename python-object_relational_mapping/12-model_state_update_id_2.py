0a8e3cbce3746c5ccface8cc8356e9bfa89c29d3
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-11 15:17:34 -0500

    fix: query wasnt returning State instance

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -29,6 +29,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.id == 2)
+    state = session.query(State).filter(State.id == 2).first()
     state.name = "New Mexico"
     session.commit()
