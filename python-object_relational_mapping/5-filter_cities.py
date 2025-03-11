43ed3372e7873a5b5898123d494f8045b1cca107
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-10 20:34:14 -0500

    fix: prefixed `cities` to `name`

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
         charset="utf8"
     )
     cur = db.cursor()
-    cur.execute("SELECT `name` FROM `cities`\
+    cur.execute("SELECT `cities`.`name` FROM `cities`\
                 JOIN `states` ON `cities`.`state_id` = `states`.`id`\
                 WHERE `states`.`name` = %s\
                 ORDER BY `cities`.`id` ASC", (sys.argv[4],)
