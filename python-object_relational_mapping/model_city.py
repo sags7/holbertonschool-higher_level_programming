c75cc2423403c322af364ab1ac6f7dbfa8549013
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-11 17:30:01 -0500

    fix: typo in id column

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -15,8 +15,9 @@ can’t be null and is a foreign key to states.id
 You must use the module SQLAlchemy
 """
 
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
@@ -30,6 +31,6 @@ class City(Base):
     state_id (sqlalchemy.Integer): The city's state id
     """
     __tablename__ = "cities"
-    id = Column(Integer, primary_jey=True, nullable=False, auto_increment=True)
+    id = Column(Integer, primary_key=True, nullable=False, auto_increment=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
