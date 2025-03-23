82ccc11fcd51d1c9fd21e90256b7388f151063f9
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-23 13:29:35 -0500

    add solution to task00

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+def generate_invitations(template, attendees):
+    """
+    Generates invitation letters for attendees based on the provided template.
+    attendees is a list of dictionaries, where each dictionary contains the
+    name, event_title, event_date, and event_location of the attendee.  
+    template is a string that contains placeholders for the name, event_title,
+    event_date, and event_location.  The placeholders are enclosed in curly braces.
+    The function will generate an invitation letter for each attendee by replacing
+    the placeholders with the actual values.  The function will write each
+    invitation letter to a file named 'invitation_X.txt', where X is the index of
+    the attendee in the attendees list.
+    """
+
+    if not isinstance(template, str):
+        print('Error: provided template is not a string')
+        return
+    if not isinstance(attendees, list):
+        print('Error: provided attendees is not a list')
+        return
+    if not template:
+        print('Error: provided template is empty')
+        return
+    if not attendees:
+        print('Error: provided attendees list is empty')
+        return
+
+    for index, attendee in enumerate(attendees, start=1):
+        invitation = template
+        for key in ['name', 'event_title', 'event_date', 'event_location']:
+            if key in attendee:
+                invitation = invitation.replace(
+                    '{' + key + '}', attendee[key] or 'N/A')
+
+        filename = f'invitation_{index}.txt'
+        with open(filename, 'w') as file:
+            file.write(invitation)
